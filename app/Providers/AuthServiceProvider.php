7bacb187dfacf076f39a6c33a8d9b7d6237fc819
Giovanni Stroppa <giosfdev@gmail.com>
2019-09-28 23:21:16 -0300

    Implementa user login e autenticacao de roles em views

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Post;
+use App\Policies\PostPolicy;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +16,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\ModelPolicy',
+        Post::class => PostPolicy::class,
     ];
 
     /**
@@ -21,9 +25,17 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(GateContract $gate)
     {
-        $this->registerPolicies();
+        $this->registerPolicies($gate);
+
+        $gate->define('isAdmin', function($user){
+            return $user->tipo_usuario == 'admin';
+        });
+
+        $gate->define('isEditor', function($user){
+            return $user->tipo_usuario == 'editor';
+        });
 
         //
     }
