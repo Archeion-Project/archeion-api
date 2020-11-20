4e76c1b44ae26a013bf984a6238109ba65498189
Giovanni Stroppa <giosfdev@gmail.com>
2020-11-20 19:28:35 -0300

    Exclui filtros de barra de busca

--- app/Http/Executar/PesquisaFicha.php
+++ app/Http/Executar/PesquisaFicha.php
@@ -24,28 +24,11 @@ class PesquisaFicha
 	{
 		$parameters = request()->request;
 		$this->realizaBusca = DB::table('fichas')->select('*');
-		$filtro = $parameters->get('filtro');
-
-		if($filtro == 'assunto')
-		{
-			// dd('ass');
-			$this->resultadosBusca = $this->realizaBusca->orWhere('assunto', 'like', '%%' . $this->termoBusca . '%%')->get();
-		}
-		if($filtro == 'resumo')
-		{
-			// dd('res');
-			$this->resultadosBusca = $this->realizaBusca->orWhere('resumo', 'like', '%%' . $this->termoBusca . '%%')->get();
-		}
-		if($filtro == 'periodico')
-		{
-			// dd('per');
-			$this->resultadosBusca = $this->realizaBusca->orWhere('periodico', 'like', '%%' . $this->termoBusca . '%%')->get();
-		}
-		if($filtro == 'comentario')
-		{
-			// dd('com');
-			$this->resultadosBusca = $this->realizaBusca->orWhere('comentarios', 'like', '%%' . $this->termoBusca . '%%')->get();
-		}
+		$this->resultadosBusca = $this->realizaBusca
+			->orWhere('assunto', 'like', '%%' . $this->termoBusca . '%%')
+			->orWhere('resumo', 'like', '%%' . $this->termoBusca . '%%')
+			->orWhere('periodico', 'like', '%%' . $this->termoBusca . '%%')
+			->orWhere('comentarios', 'like', '%%' . $this->termoBusca . '%%')->get();
 	}
 
 	public function view(): \Illuminate\View\View
--- resources/views/componentes/barraBusca.blade.php
+++ resources/views/componentes/barraBusca.blade.php
@@ -5,18 +5,19 @@
 		<div class="container form-busca">
 			<div class="form-group">
 				<input type="text" class="form-control" name="termoBusca" placeholder="Busque no acervo" aria-label="Recipient's username" aria-describedby="button-addon2" autofocus>
-				<p>Filtrar busca por:</p>
+				<!-- <p>Filtrar busca por:</p>
 				<div class="form-check form-check-inline">
 					<input class="form-check-input form-check" type="radio" id="inlineCheckbox1" name="filtro" value="assunto" checked>
-					<label class="form-check-label" for="inlineCheckbox1">Assunto </label>
+						<label class="form-check-label" for="inlineCheckbox1">Assunto </label>
 					<input class="form-check-input form-check" type="radio" id="inlineCheckbox2" name="filtro" value="periodico">
-					<label class="form-check-label" for="inlineCheckbox2">Periódico </label>
+						<label class="form-check-label" for="inlineCheckbox2">Periódico </label>
 					<input class="form-check-input form-check" type="radio" id="inlineCheckbox3" name="filtro" value="resumo">
-					<label class="form-check-label" for="inlineCheckbox3">Resumo </label>
+						<label class="form-check-label" for="inlineCheckbox3">Resumo </label>
 					<input class="form-check-input form-check" type="radio" id="inlineCheckbox4" name="filtro" value="resumo">
-					<label class="form-check-label" for="inlineCheckbox4">Comentários </label>
+						<label class="form-check-label" for="inlineCheckbox4">Comentários </label>
 				</div>
-				<div>
+				<div> -->
+				<br>
 					<button type="submit" class="btn btn-primary">Pesquisar</button>
 				</div>
 			</div>
