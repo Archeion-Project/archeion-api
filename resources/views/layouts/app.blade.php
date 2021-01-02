c2b5f070e751f9d26940ccf65e7d3185f1c2b1b7
Giovanni Stroppa <giosfdev@gmail.com>
2021-01-02 17:49:45 -0300

    Implementa scripts para datepicker - remove js/app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,6 @@
 
 		<title>{{ config('app.name', 'Laravel') }}</title>
 
-		<!-- Scripts -->
-		<script
-			  src="https://code.jquery.com/jquery-3.5.1.js"
-			  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-			  crossorigin="anonymous"></script>
-		<script src="{{ asset('js/app.js') }}" defer></script>
-
 		<!-- Fonts -->
 		<link rel="dns-prefetch" href="//fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css?family=Nunito|Source+Code+Pro|IBM+Plex+Mono|Raleway:300&display=swap" rel="stylesheet">
@@ -24,6 +17,12 @@
 		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 		<link href="{{ asset('css/wb.css') }}" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<!-- Scripts -->
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+		<script src="//code.jquery.com/jquery-1.12.4.js"></script>
+		<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+		<!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 		
 	</head>
 
@@ -51,21 +50,89 @@
 				var button = $(event.relatedTarget) // Button that triggered the modal
 				var recipient = button.data('whatever') // Extract info from data-* attributes
 				// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-				console.log(recipient)
 				// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
 				var modal = $(this)
-
 				modal.find('#modal-assunto').val(recipient.assunto)
-				modal.find('#modal-periodico').val(recipient.periodico)
+				modal.find('#modal-periodico').val(recipient.periodico_id)
 				modal.find('#modal-data_edicao').val(recipient.data_edicao)
 				modal.find('#modal-duracao_edicao').val(recipient.duracao_edicao)
 				modal.find('#modal-pagina').val(recipient.pagina)
 				modal.find('#modal-resumo').val(recipient.resumo)
 				modal.find('#modal-comentario').val(recipient.comentario)
 
+				//Substitui id da ficha em modal-form > action > route
+				var str = $('#modal-form').attr('action');
+				var i = str.lastIndexOf('/');
+				if (i != -1) {
+					str = str.substr(0, i + 1) + recipient.id;
+				}
+				modal.find('#modal-form').attr('action', str)
+
 			})
 
-			// $(document).on('click', '#editar-item-grid', function (event) {
+			$('#apagar-modal').on('shown.bs.modal', function (event) {
+
+				var button = $(event.relatedTarget) // Button that triggered the modal
+				console.log(button)
+				var recipient = button.data('whatever') // Extract info from data-* attributes
+				// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+				var modal = $(this)
+
+				//Substitui id da ficha em modal-form > action > route
+				var str = $('#modal-form').attr('action');
+				var i = str.lastIndexOf('/');
+				if (i != -1) {
+					str = str.substr(0, i + 1) + recipient.id;
+				}
+				modal.find('#modal-form').attr('action', str)
+
+			})
+
+			$.datepicker.setDefaults({
+				dateFormat: 'dd/mm/yy',
+				changeMonth: true,
+				changeYear: true,
+				showOtherMonths: true,
+				selectOtherMonths: true,
+				dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'],
+				dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+				dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+				monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+				monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+				yearRange: '1500:'+(new Date).getFullYear()
+			});
+
+			$( function() {
+				var dateFormat = "dd/mm/yy",
+					from = $( "#data_edicao" )
+						.datepicker({
+							defaultDate: "+1w",
+							changeMonth: true,
+						})
+						.on( "change", function() {
+							to.datepicker( "option", "minDate", getDate( this ) );
+						}),
+					to = $( "#duracao_edicao" ).datepicker({
+						defaultDate: "+1w",
+						changeMonth: true,
+					})
+					.on( "change", function() {
+						from.datepicker( "option", "maxDate", getDate( this ) );
+					});
+
+				function getDate( element ) {
+					var date;
+					try {
+						date = $.datepicker.parseDate( dateFormat, element.value );
+					} catch( error ) {
+						date = null;
+					}
+
+					return date;
+				}
+			});
+	
+	// $(document).on('click', '#editar-item-grid', function (event) {
 
 			// 	$(this).addClass('edit-item-trigger-clicked'); //useful for identifying which trigger was clicked and consequently grab data from the correct row and not the wrong one.
 
